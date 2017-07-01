c0825f87db7da31c3bdf7447d33980b121f2b347
Hank Tom <hanktom@gmail.com>
2017-07-01 16:37:11 +0800

    SQLite-0

--- app/src/main/java/com/litto/expense/AddActivity.java
+++ app/src/main/java/com/litto/expense/AddActivity.java
@@ -1,5 +1,6 @@
 package com.litto.expense;
 
+import android.content.ContentValues;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +11,7 @@ public class AddActivity extends AppCompatActivity {
     private EditText edDate;
     private EditText edAmount;
     private EditText edInfo;
+    private ExpenseHelper helper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +20,20 @@ public class AddActivity extends AppCompatActivity {
         edDate = (EditText) findViewById(R.id.ed_date);
         edAmount = (EditText) findViewById(R.id.ed_amount);
         edInfo = (EditText) findViewById(R.id.ed_info);
+        helper = new ExpenseHelper(this);
     }
 
     public void add(View v){
         String date = edDate.getText().toString();
         int amount = Integer.parseInt(edAmount.getText().toString());
         String info = edInfo.getText().toString();
-
+        ContentValues values = new ContentValues();
+        values.put("cdate", date);
+        values.put("amount", amount);
+        values.put("info", info);
+        long id = helper.getWritableDatabase().insert("expense", null, values);
+        if (id != -1 ){
+            //TODO: if insert successful
+        }
     }
 }
--- app/src/main/java/com/litto/expense/ExpenseHelper.java
+++ app/src/main/java/com/litto/expense/ExpenseHelper.java
@@ -3,6 +3,7 @@ package com.litto.expense;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by hank on 2017/7/1.
@@ -12,6 +13,7 @@ public class ExpenseHelper extends SQLiteOpenHelper {
 
     private static final String DB_NAME = "expense.db";
     private static final int DB_VERSION = 1;
+    private static final String TAG = ExpenseHelper.class.getSimpleName();
 
     public ExpenseHelper(Context context) {
         super(context, DB_NAME, null , DB_VERSION);
@@ -24,6 +26,7 @@ public class ExpenseHelper extends SQLiteOpenHelper {
                 "cdate DATETIME NOT NULL, " +
                 "info VARCHAR, " +
                 "amount INTEGER)");
+        Log.d(TAG, "onCreate: ");
     }
 
     @Override
