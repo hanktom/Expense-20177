cef19f3db4e816e28c3fd7a23762b6411ea5c167
Hank Tom <hanktom@gmail.com>
2017-07-08 13:31:08 +0800

    Database Contracts-1

---
+++ app/src/main/java/com/litto/expense/ExpenseContract.java
@@ -0,0 +1,14 @@
+package com.litto.expense;
+
+/**
+ * Created by hank on 2017/7/8.
+ */
+
+public class ExpenseContract {
+    public static final String TABLE_EXPENSE = "expense";
+    public static final String COL_ID = "_id";
+    public static final String COL_DATE = "cdate";
+    public static final String COL_INFO = "info";
+    public static final String COL_AMOUNT = "amount";
+
+}
--- app/src/main/java/com/litto/expense/ExpenseHelper.java
+++ app/src/main/java/com/litto/expense/ExpenseHelper.java
@@ -1,6 +1,7 @@
 package com.litto.expense;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
@@ -14,20 +15,31 @@ public class ExpenseHelper extends SQLiteOpenHelper {
     private static final String DB_NAME = "expense.db";
     private static final int DB_VERSION = 1;
     private static final String TAG = ExpenseHelper.class.getSimpleName();
+    public static final String CREATE_EXPENSE_SQL = "CREATE TABLE " + ExpenseContract.TABLE_EXPENSE +
+            "(" + ExpenseContract.COL_ID + " INTEGER PRIMARY KEY, " +
+            ExpenseContract.COL_DATE + " DATETIME NOT NULL, " +
+            ExpenseContract.COL_INFO + " VARCHAR, " +
+            ExpenseContract.COL_AMOUNT + " INTEGER)";
+    private Resources resources;
+//    private Context context;
 
     public ExpenseHelper(Context context) {
         super(context, DB_NAME, null , DB_VERSION);
+//        this.context = context;
+        resources = context.getResources();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE main.expense " +
-                "(_id INTEGER PRIMARY KEY, " +
-                "cdate DATETIME NOT NULL, " +
-                "info VARCHAR, " +
-                "amount INTEGER)");
+        db.execSQL(CREATE_EXPENSE_SQL);
         Log.d(TAG, "onCreate: ");
+        readExpensesFromResources(db);
     }
+
+    private void readExpensesFromResources(SQLiteDatabase db) {
+//        resources.raw
+    }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
